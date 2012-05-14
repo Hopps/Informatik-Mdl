32af85071b051091bcd862c25c11acfddd72cecd
Hopps <Hopps@Hopps-PC>
2012-05-14 14:04:45 +0200

    TestCase umgeschrieben für BinBaum -> 3. Teil

--- datenstrukturen/TestCase.java
+++ datenstrukturen/TestCase.java
@@ -21,8 +21,37 @@ public class TestCase {
         baum.linkerTeilbaum().setzeRechts(new BinBaum(10));
         baum.rechterTeilbaum().setzeLinks(new BinBaum(3));
         baum.linkerTeilbaum().rechterTeilbaum().setzeRechts(new BinBaum(2));
+        System.out.println("Preorder");
+        preorder(baum);
+        System.out.println("Inorder");
+        inorder(baum);
+        System.out.println("Postorder");
+        postorder(baum);
     }
 
+    private void preorder(BinBaum pKnoten) {
+        System.out.println(pKnoten.inhalt());
+        if(pKnoten.linkerTeilbaum()!=null)
+            preorder(pKnoten.linkerTeilbaum());
+        if(pKnoten.rechterTeilbaum()!=null)
+            preorder(pKnoten.rechterTeilbaum());
+    }
+
+    private void inorder(BinBaum pKnoten) {
+        if(pKnoten.linkerTeilbaum()!=null)
+            inorder(pKnoten.linkerTeilbaum());
+        System.out.println(pKnoten.inhalt());
+        if(pKnoten.rechterTeilbaum()!=null)
+            inorder(pKnoten.rechterTeilbaum());
+    }
+
+    private void postorder(BinBaum pKnoten) {
+        if(pKnoten.linkerTeilbaum()!=null)
+            postorder(pKnoten.linkerTeilbaum());
+        if(pKnoten.rechterTeilbaum()!=null)
+            postorder(pKnoten.rechterTeilbaum());
+        System.out.println(pKnoten.inhalt());
+    }
 
 
 
