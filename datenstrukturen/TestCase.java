0312acf661bee67ecdb4596c291fe09b86149059
Hopps <Hopps@Hopps-PC>
2012-05-14 13:50:07 +0200

    TestCase umgeschrieben für BinBaum -> 2. Teil

--- datenstrukturen/TestCase.java
+++ datenstrukturen/TestCase.java
@@ -10,13 +10,17 @@ public class TestCase {
 
     public static void main(String[] args) {
         TestCase tc = new TestCase();
-        tc.erstelleBinBaum();
+        tc.erstelleBaum();
     }
 
     private void erstelleBaum() {
-        baum = new BinBaum();
-
-
+        baum = new BinBaum(5);
+        baum.setzeLinks(new BinBaum(4));
+        baum.setzeRechts(new BinBaum(7));
+        baum.linkerTeilbaum().setzeLinks(new BinBaum(8));
+        baum.linkerTeilbaum().setzeRechts(new BinBaum(10));
+        baum.rechterTeilbaum().setzeLinks(new BinBaum(3));
+        baum.linkerTeilbaum().rechterTeilbaum().setzeRechts(new BinBaum(2));
     }
 
 
